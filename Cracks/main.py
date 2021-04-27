1a0a2e2fb19eaab0f35bf5707a632a0602021669
kaer <kaer@joergensenfam.dk>
2021-04-27 11:08:35 +0200

    comments

--- Cracks/main.py
+++ Cracks/main.py
@@ -4,33 +4,52 @@ import isolate
 from matplotlib import pyplot as plt
 import numpy as np
 
+#load images
+images = [cv2.imread(file) for file in glob.glob("f:/git 2/Cracks/2/*.jpg")]
 
-images = [cv2.imread(file) for file in glob.glob("f:/cracks/*.jpg")]
+#declare vectors
 con = []
 hsv = []
 Tjek = []
 im = []
 im2 = []
 im3 = []
+#kernel for morphology
 kernelEllipse = cv2.getStructuringElement(cv2.MORPH_CROSS, (3, 3))
 kernelEllipse2 = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
+
+#for loop for looping through the images
 for i in range(len(images)):
+
+    #isolate backplate and resize images
     images[i] = isolate.findBackPlate(images[i])
     images[i] = cv2.resize(images[i], (int(images[i].shape[1] / 3), int(images[i].shape[0] / 3)))
+
+    #convert to HSV
     hsv.append(cv2.cvtColor(images[i], cv2.COLOR_BGR2HSV))
+
+    #empty pics for contour
     con.append(np.zeros(hsv[i].shape))
     #Tjek.append(cv2.inRange(hsv[i], (100, 25, 30), (170, 130, 80)))
+
+    #thredshold
     Tjek.append(cv2.inRange(hsv[i], (0, 0, 0), (255, 255, 43)))
+
+    #morphology
     im.append(cv2.morphologyEx(Tjek[i], cv2.MORPH_ERODE, kernelEllipse))
-    # Tjek.append(cv2.inRange(hsv[i], (0, 0, 0), (36, 100, 255)))
     im2.append(cv2.morphologyEx(im[i], cv2.MORPH_DILATE, kernelEllipse2))
     #im3.append(cv2.morphologyEx(im2[i], cv2.MORPH_CLOSE, kernelEllipse2))
+
+    #find contour
     contours, hierarchy = cv2.findContours(im2[i], cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    #draw contour in empty pics
     cv2.drawContours(con[i], contours, -1, (0, 255, 0), 3)
+
+    #show pic/contour
     cv2.imshow("ss", con[i])
     print("AHH SHIET")
     cv2.waitKey(0)
-    # Creates a morphology kernel of 7x7 size in the shape of an ellipse
     # plt.subplot(4, 3, i+1), plt.imshow(hsv[i])
 # plt.show()
 
