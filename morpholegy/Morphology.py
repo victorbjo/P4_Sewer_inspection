8e628995dc4d514dfe62c1dbaee2557d12c16674
JimmiJensen <jijens19@student.aau.dk>
2021-05-10 00:47:54 +0200

    Eclipse now also works for dilate
    it takes more than five minutes to run though

--- morpholegy/Morphology.py
+++ morpholegy/Morphology.py
@@ -80,8 +80,10 @@ def erode(img, k_x, k_y, k_type):
 def dilate(img, k_x, k_y, k_type): #works excalty like erode, but instead of checking for zeros in kernal checks for 255 aka white, and the new image is not filled with white
     
     image = np.zeros(img.shape)
-    
+    counter = 0
     for x in range(img.shape[0]):
+        counter = counter +1
+        print(counter)
         for y in range(img.shape[1]):
             if img[x][y] == 255:
                 image[x][y] = 255
@@ -99,25 +101,32 @@ def dilate(img, k_x, k_y, k_type): #works excalty like erode, but instead of che
                                 elif img[x+i][y+j] == 255:
                                     image[x][y] = 255
                             elif k_type == "eclipse":                                    
-                                if img[x-i+1][y-k_y/2] == 255:
+                                    
+                                if img[x-i+1][y-j+1] == 255:
                                     image[x][y] = 255
-                                elif img[x-i+1][y+k_y/2] == 255:
+                                elif img[x-i+1][y+j-1] == 255:
                                     image[x][y] = 255
-                                elif img[x+i-1][y-k_y/2] == 255:
+                                elif img[x+i-1][y-j+1] == 255:
                                     image[x][y] = 255
-                                elif img[x+i-1][y+k_y/2] == 255:
+                                elif img[x+i-1][y+j-1] == 255:
                                     image[x][y] = 255
 
-                                if img[x-i+1][y-k_y/2+1] == 255:
+                                elif img[x-1][y-int(k_y/2)] == 255:
                                     image[x][y] = 255
-                                elif img[x-i+1][y+k_y/2] == 255:
+                                elif img[x][y-int(k_y/2)] == 255:
                                     image[x][y] = 255
-                                elif img[x+i-1][y-k_y/2] == 255:
+                                elif img[x+1][y-int(k_y/2)] == 255:
                                     image[x][y] = 255
-                                elif img[x+i-1][y+k_y/2] == 255:
+
+                                elif img[x-1][y+int(k_y/2)] == 255:
                                     image[x][y] = 255
+                                elif img[x][y+int(k_y/2)] == 255:
+                                    image[x][y] = 255
+                                elif img[x+1][y+int(k_y/2)] == 255:
+                                    image[x][y] = 255
+                          
                                     
-                                if img[x-int(k_x/2)][y] == 255:
+                                elif img[x-int(k_x/2)][y] == 255:
                                     image[x][y] = 255
                                 elif img[x+int(k_x/2)][y] == 255:
                                     image[x][y] = 255
@@ -160,10 +169,10 @@ image = cv2.imread("3_coating.PNG")
 
 thresh = cv2.inRange(image, (100,20,0), (140,255,255))
 
-img = morphol0gy(thresh, "erode", 9,9,"eclipse")
+img = morphol0gy(thresh, "dilate", 9,9,"eclipse")
 kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (9,9))
 print(kernel)
-test = cv2.morphologyEx(thresh, cv2.MORPH_ERODE, kernel)
+test = cv2.morphologyEx(thresh, cv2.MORPH_DILATE, kernel)
 print(img.shape)
 #cv2.imshow("threshold", thresh)
 cv2.imshow("morph me", img)
