3632ea748756889a4cd59bb8aeb52b570174de38
kaer <kaer@joergensenfam.dk>
2021-05-12 13:30:37 +0200

    FKP

--- AllCombined/mainCracks.py
+++ AllCombined/mainCracks.py
@@ -16,14 +16,17 @@ def findCracks(pic):
     im = []
     im2 = []
     im3 = []
-    knak = 0
+
     #kernel for morphology
     kernelEllipse = cv2.getStructuringElement(cv2.MORPH_CROSS, (3, 3))
     kernelEllipse2 = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
 
     #for loop for looping through the images
     for i in range(len(images)):
-       # print(i+1)
+        # print(i+1)
+        knak = 0
+
+
         cv2.destroyAllWindows()
         #isolate backplate and resize images
         images[i] = isolate.findBackPlate(images[i])
@@ -37,7 +40,7 @@ def findCracks(pic):
         #Tjek.append(cv2.inRange(hsv[i], (100, 25, 30), (170, 130, 80)))
 
         #thredshold
-        Tjek.append(cv2.inRange(hsv[i], (0, 30, 0), (255, 255, 43)))
+        Tjek.append(cv2.inRange(hsv[i], (30, 120, 0), (179, 255, 50)))
 
         #morphology
         im.append(cv2.morphologyEx(Tjek[i], cv2.MORPH_ERODE, kernelEllipse))
@@ -46,23 +49,38 @@ def findCracks(pic):
 
         #find contour
         contours, hierarchy = cv2.findContours(im2[i], cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        area = []
-        arc = []
+        area = np.zeros(len(contours))
+        arc = np.zeros(len(contours))
+        circularity = np.zeros(len(contours))
         #find area
         if contours:
             for j in range (len(contours)):
-                if 350 < cv2.contourArea(contours[j]) < 800:
-                    area.append(cv2.contourArea(contours[j]))
-                    if cv2.arcLength(contours[j], True) < 200:
-                        arc.append(cv2.arcLength(contours[j], True))
-                        # draw contour in empty pics
-                        cv2.drawContours(images[i], contours, j, (0, 255, 0), 1)
-                        #print("AHH SHIET")
-                        #print(area)
-                        #print(arc)
-                        #cv2.imshow("ss", images[i])
-                        Knak = 1
-                        cv2.waitKey(0)
+                if 50 < cv2.contourArea(contours[j]) < 3000:
+                    area[j] = cv2.contourArea((contours[j]))
+                    arc[j] = cv2.arcLength(contours[j], 1)
+                    circularity[j] = (4 * 3.14 * area[j]) / (int(arc[j]) ** 2)
+                    #print("circularity: ", circularity)
+                    #print("area", area)
+                    #print("arc", arc)
+                    cv2.drawContours(images[i], contours, j, (0, 255, 0), 1)
+                    #cv2.imshow("bin", Tjek[i])
+                    cv2.imshow("ss", images[i])
+                    knak = 1
+                    cv2.waitKey(0)
+                # if 350 < cv2.contourArea(contours[j]) < 2000: #800
+                #     area.append(cv2.contourArea(contours[j]))
+                #     if cv2.arcLength(contours[j], True) < 2000: #200
+                #         arc.append(cv2.arcLength(contours[j], True))
+                #         # draw contour in empty pics
+                #         cv2.drawContours(images[i], contours, j, (0, 255, 0), 1)
+                #         #print("AHH SHIET")
+                #
+                #         # print(circularity[j])
+                #         # print(area)
+                #         # print(arc)
+                #         cv2.imshow("ss", images[i])
+                #         knak = 1
+                #
           #          else:
                         #print("No cracks found!!!")
             #            cv2.waitKey(0)
@@ -81,6 +99,16 @@ def findCracks(pic):
         #cv2.imshow("ss", con[i])
     cv2.waitKey(0)
     return (knak)
+
+# pics = []
+# pics = [cv2.imread(file) for file in glob.glob("f:/Crack/*.jpg")]
+# for t in range (len(pics)):
+#     #print(t+1)
+#     pic = pics[t]
+#     findCracks(pic)
+
+
+
         # plt.subplot(4, 3, i+1), plt.imshow(hsv[i])
     # plt.show()
 
